9b770ea3ab70598563e5347d59d2c3aaf089fbbb
shldlqj2 <shldlqj2@naver.com>
2025-04-05 17:20:27 +0900

    test4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,15 +3,25 @@
  */
 
 plugins {
-    id("buildlogic.java-application-conventions")
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.spring") version "1.9.20"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
 }
 
 dependencies {
-    implementation("org.apache.commons:commons-text")
-    implementation(project(":utilities"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-application {
-    // Define the main class for the application.
-    mainClass = "org.example.app.App"
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
